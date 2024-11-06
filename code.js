a5e68d3ac8f555314888ee1667492e1f1863185f
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-11-06 15:05:15 -0700

    Update code.js
    Adding a factorial function.

--- code.js
+++ code.js
@@ -7,12 +7,20 @@
 //     if(n === 0) return 1;
 //     else return 1.0 / factorial(n) + e(n - 1);
 // }
-
+function factorial (n) {
+    let fact = 1;
+    
+    for (i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    
+    return fact;
+}
 function e(n) {
     let factorial = 1;
     let euler = 0;
     
-    for (let i = 0; i <= n; i++){
+    for (let i = 0; i <= n; i++) {
       if (i > 0) factorial *= i;
       
       euler += 1 / factorial;
