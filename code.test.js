080ed13aada81d01b974047a18b4477cabc50834
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-11-06 16:10:13 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -2,11 +2,6 @@ const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
-
-assert(factorial(10) === 3628800);
-
-assert(e(10) === 2.7182818011463845);
-
 assert(factorial(10) === 3628800);
 
 assert(e(10) === 2.7182818011463845);
@@ -16,8 +11,6 @@ assert(factorial(1) === 1);
 assert(factorial(2) === 2);
 assert(factorial(3) === 6);
 assert(factorial(4) === 24);
-
-
 
 assert(e(5) ===  2.7166666666666663);
 assert(e(6) ===  2.7180555555555554);
